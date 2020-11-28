6fe5e8e295b9dc7f41a9256f7cbbad5858a57e1c
thomas.lf <thomas.lefloch@succubus.fr>
2020-11-28 22:37:36 +0100

    recovering user input without dialog

--- src/extension.ts
+++ src/extension.ts
@@ -2,25 +2,24 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+function getTargetChar({ subscriptions }: vscode.ExtensionContext): Promise<String> {
+  return new Promise(resolve => {
+    const targetCharTrap = vscode.commands.registerCommand('type', e => {
+      targetCharTrap.dispose();
+      resolve(e.text);
+    });
+    subscriptions.push(targetCharTrap);
+  });
+}
+
 export function activate(context: vscode.ExtensionContext) {
+  //
+  let disposable = vscode.commands.registerCommand('delete-inside.delete', async () => {
+    let targetChar = await getTargetChar(context);
+    vscode.window.showInformationMessage('target char: ' + targetChar);
+  });
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "delete-inside" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('delete-inside.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from delete-inside!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
