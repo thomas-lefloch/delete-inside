68baed6daefaa15159e6f5a88756599d00026dad
thomas.lf <thomas.lefloch@succubus.fr>
2020-11-30 00:48:28 +0100

    simplification

--- src/extension.ts
+++ src/extension.ts
@@ -12,7 +12,7 @@ function getTargetChar({ subscriptions }: vscode.ExtensionContext): Promise<stri
   });
 }
 
-const matchingPair = (char: string) => {
+const getMatchingPair = (char: string) => {
   switch (char) {
     case "'":
       return char;
@@ -30,7 +30,7 @@ const matchingPair = (char: string) => {
 };
 
 // TODO: manage recursive character like (afjfk())
-// TODO: reports errors to user, add settings to enable silent fail ??
+// TODO: reports errors to user, and add settings to enable silent fail ??
 // TODO: ignore escaped character like "jflskjf\"fjdslf\"jfsdlf"
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand('delete-inside.delete-inside', async () => {
@@ -39,39 +39,42 @@ export function activate(context: vscode.ExtensionContext) {
     if (!editor.selection.isEmpty) return;
 
     const targetChar = await getTargetChar(context);
-    const matchingChar = matchingPair(targetChar);
-    if (!matchingChar) return; // Char not supported
+    const matchingChar = getMatchingPair(targetChar);
+    if (!matchingChar) return; // Character not supported
 
     // Do search in async  ?? need big files to test it
-    let startIndex: vscode.Position | undefined;
+    let startPosition: vscode.Position | undefined;
     let startLine = editor.selection.start.line;
-    while (startLine >= 0 && !startIndex) {
-      const lineContent = editor.document.lineAt(startLine).text;
-      let startSearchAt;
-      if (startLine === editor.selection.start.line)
-        startSearchAt = editor.selection.start.character;
-      const index = lineContent.lastIndexOf(targetChar, startSearchAt);
-      if (index >= 0) startIndex = new vscode.Position(startLine, index + 1);
+    let char = editor.selection.start.character;
+    let lineContent = editor.document.lineAt(startLine).text;
+    while (startLine >= 0 && !startPosition) {
+      while (char >= 0 && !startPosition) {
+        if (lineContent[char] === targetChar)
+          startPosition = new vscode.Position(startLine, char + 1);
+        char--;
+      }
       startLine--;
+      if (startLine >= 0) lineContent = editor.document.lineAt(startLine).text;
     }
-    if (!startIndex) return; // character not found
+    if (!startPosition) return; // character not found
 
-    let endIndex: vscode.Position | undefined;
+    let endPosition: vscode.Position | undefined;
     let endLine = editor.selection.start.line;
-    while (endLine < editor.document.lineCount && !endIndex) {
-      const lineContent = editor.document.lineAt(endLine).text;
-      let startSearchAt;
-      if (endLine === editor.selection.start.line) startSearchAt = editor.selection.start.character;
-
-      const index = lineContent.indexOf(matchingChar, startSearchAt);
-      if (index >= 0) endIndex = new vscode.Position(endLine, index);
+    char = editor.selection.start.character;
+    lineContent = editor.document.lineAt(endLine).text;
+    while (endLine < editor.document.lineCount && !endPosition) {
+      while (char < lineContent.length && !endPosition) {
+        if (lineContent[char] === matchingChar) endPosition = new vscode.Position(endLine, char);
+        char++;
+      }
       endLine++;
+      if (endLine < editor.document.lineCount) lineContent = editor.document.lineAt(endLine).text;
     }
-    if (!endIndex) return; // character not found
+    if (!endPosition) return; // matching character not found
 
     editor.edit(editBuilder => {
-      if (!endIndex || !startIndex) return; // to shutup typescript compiler
-      editBuilder.delete(new vscode.Range(startIndex, endIndex));
+      if (!endPosition || !startPosition) return; // to shutup typescript compiler
+      editBuilder.delete(new vscode.Range(startPosition, endPosition));
     });
   });
 
